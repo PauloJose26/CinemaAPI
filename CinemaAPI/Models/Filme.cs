31c64085488e1a6efd5463982ce34410bb9e80b4
PauloJose26 <paulojoliveira.2616@gmail.com>
2026-01-07 10:33:20 -0300

    Criação da classe Filme

---
+++ CinemaAPI/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaAPI.Models;
+
+public class Filme
+{
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Duracao { get; set; }
+
+    public Filme(string titulo, string genero, int duracao)
+    {
+        this.Titulo = titulo;
+        this.Genero = genero;
+        this.Duracao = duracao;
+    }
+}
