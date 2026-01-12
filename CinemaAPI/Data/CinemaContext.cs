1d181f9c05b8c8135db31a5899a7f6bfe438a8f0
PauloJose26 <paulojoliveira.2616@gmail.com>
2026-01-12 13:37:12 -0300

    Delete cascade restrict

--- CinemaAPI/Data/CinemaContext.cs
+++ CinemaAPI/Data/CinemaContext.cs
@@ -8,9 +8,9 @@ public class CinemaContext : DbContext
 {
     public CinemaContext(DbContextOptions options) : base(options) { }
 
-    /*
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        /*
         modelBuilder.Entity<Sessao>()
             .HasKey(sessao => new { sessao.FilmeId, sessao.CinemaId });
 
@@ -23,8 +23,13 @@ public class CinemaContext : DbContext
             .HasOne(sessao => sessao.Filme)
             .WithMany(filme => filme.Sessoes)
             .HasForeignKey(sessao => sessao.FilmeId);
+        */
+
+        modelBuilder.Entity<Endereco>()
+            .HasOne(endereco => endereco.Cinema)
+            .WithOne(cinema => cinema.Endereco)
+            .OnDelete(DeleteBehavior.Restrict);
     }
-    */
 
     public DbSet<Filme> Filmes { get; set; }
     public DbSet<Cinema> Cinemas { get; set; }
