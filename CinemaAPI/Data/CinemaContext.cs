d94f10557c51fa9ad8807ada7eb708b0e2668573
PauloJose26 <paulojoliveira.2616@gmail.com>
2026-01-12 12:45:25 -0300

    OnModelCreating

--- CinemaAPI/Data/CinemaContext.cs
+++ CinemaAPI/Data/CinemaContext.cs
@@ -8,6 +8,24 @@ public class CinemaContext : DbContext
 {
     public CinemaContext(DbContextOptions options) : base(options) { }
 
+    /*
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Sessao>()
+            .HasKey(sessao => new { sessao.FilmeId, sessao.CinemaId });
+
+        modelBuilder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Cinema)
+            .WithMany(cinema => cinema.Sessoes)
+            .HasForeignKey(sessao => sessao.CinemaId);
+
+        modelBuilder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Filme)
+            .WithMany(filme => filme.Sessoes)
+            .HasForeignKey(sessao => sessao.FilmeId);
+    }
+    */
+
     public DbSet<Filme> Filmes { get; set; }
     public DbSet<Cinema> Cinemas { get; set; }
     public DbSet<Endereco> Enderecos { get; set; }
